02fd0075528bd5386b8af6b57393d30f35954234
Robert Cunningham <rgc3dev@gmail.com>
2016-01-28 09:22:54 -0500

    add loop to output images in an array, add plane image

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,7 @@ class WelcomeController < ApplicationController
   def index
   	@homeland = 'United States'
   	@countries = ['Brazil', 'Thailand', 'Italy']
+  	@images = ['plane.png', 'brazil.jpg', 'thailand.jpg', 'venice.jpg']
   end
 
   def about
